4d5d276d11e5cff5a6576a3788e2af31193e06a3
Arathi Gnanodayan <gnanodayanaa@vcu.edu>
2019-09-05 20:57:08 -0400

    Add project files.

---
+++ INFO 450 Homework 1/Main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int area;
+	int perimeter;
+	int length = 3;
+	int width = 2;
+
+	area = length * width;
+	perimeter = (2 * length) + (2 * width);
+
+	cout << "The area is " << area << endl;
+	cout << "The perimeter is " << perimeter << endl;
+
+	return 0;
+}
+
+
+
